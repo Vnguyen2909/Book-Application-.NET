f9c49e654541d3ad1cc8e8c769eac4d23a94e661
Vnguyen2909 <nguyenvi290903@gmail.com>
2024-08-14 15:34:22 +0700

    Application-Book Project

---
+++ BaoCao_QuanLyNhaSach/frmReportHoaDon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaoCao_QuanLyNhaSach
+{
+    public partial class frmReportHoaDon : Form
+    {
+        public frmReportHoaDon()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReportHoaDon_Load(object sender, EventArgs e)
+        {
+
+            //chạy report ko cần nhập lại mật khẩu
+
+            crystalReportViewer1.Refresh();
+            crystalReportViewer1.DisplayToolbar = false;
+            crystalReportViewer1.DisplayStatusBar = false;
+
+            crystalReportViewer1.DisplayStatusBar = false;
+            crystalReportViewer1.DisplayToolbar = true;
+            crystalReportViewer1.Refresh();
+
+        }
+
+    }
+}
---
+++ DTO/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DTO
+{
+    public class Product
+    {
+        public string MASACH { get; set; }
+        public int MALOAISACH { get; set; }
+        public string TENSACH { get; set; }
+        public int THELOAI { get; set; }
+        public int MATACGIA { get; set; }
+        public int MANHAXUATBAN { get; set; }
+        public decimal GIATIEN { get; set; }
+        public int SLTK { get; set; }
+        public string ANH { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(DataRow row)
+        {
+            MASACH = row["MASACH"].ToString();
+            MALOAISACH = Convert.ToInt32(row["MALOAISACH"]);
+            TENSACH = row["TENSACH"].ToString();
+            //THELOAI = row["THELOAI"].ToString();
+            MATACGIA = Convert.ToInt32(row["MATG"]);
+            MANHAXUATBAN = Convert.ToInt32(row["MANXB"]);
+            GIATIEN = Convert.ToDecimal(row["GIATIEN"]);
+            SLTK = Convert.ToInt32(row["SLTK"]);
+            ANH = row["ANH"].ToString();
+        }
+
+    }
+}
